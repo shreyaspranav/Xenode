f12f1c56aeddb489b0b7c25e1efbc6d235cdf8af
shreyaspranav <shrepranav5@gmail.com>
2022-08-31 11:40:43 +0530

    Removed cmake

--- SandboxApp/SandboxApp.cmake
+++
@@ -1,8 +0,0 @@
-set(SOURCES_SANDBOXAPP
-              SandboxApp/src/SandboxApp.cpp
-                    )
-
-add_executable(SandboxApp ${SOURCES_SANDBOXAPP})
-
-target_link_libraries(SandboxApp Xenode)
-target_include_directories(SandboxApp PUBLIC Xenode/src)
--- Xenode/Xenode.cmake
+++
@@ -1,6 +0,0 @@
-set(SOURCES_XENODE
-              Xenode/src/Xenode.cpp
-              Xenode/src/Xenode.h
-                    )
-
-add_library(Xenode SHARED ${SOURCES_XENODE})
