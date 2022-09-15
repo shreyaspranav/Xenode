dc07c09a65787d9815061891c87fac439b4b1427
shreyaspranav <shrepranav5@gmail.com>
2022-09-15 18:17:28 +0530

    Added glad header only opengl loader

--- Xenode/src/core/app/GameApplication.cpp
+++ Xenode/src/core/app/GameApplication.cpp
@@ -3,24 +3,33 @@
 #include "Log.h"
 #include "LayerStack.h"
 
+#define GLAD_GL_IMPLEMENTATION
+
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
 namespace Xen {
 
 	void GameApplication::Run()
 	{
-		XEN_ENGINE_LOG_ERROR_SEVERE("Hello!!");
-		XEN_ENGINE_LOG_ERROR("Hello!! {0}", 32);
-		XEN_ENGINE_LOG_WARN("Hello!!");
-		XEN_ENGINE_LOG_INFO("Hello!!");
-		XEN_ENGINE_LOG_TRACE("Hello!!");
-		
-		
-		LayerStack s(100);
 		int a = glfwInit();
 
+		GLFWwindow* window = glfwCreateWindow(600, 600, "Xenode", NULL, NULL);
+		glfwMakeContextCurrent(window);
+
 		XEN_APP_LOG_INFO("{0}", a);
-		TRIGGER_BREAKPOINT;
+		//TRIGGER_BREAKPOINT;
+
+		a = gladLoadGL(glfwGetProcAddress);
+		XEN_APP_LOG_INFO("{0}", a);
+
+		while (!glfwWindowShouldClose(window)) {
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+
+			glClearColor(0.7f, 0.9f, 0.1f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+		}
 
 	}
 }
