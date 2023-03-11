0fca37985bd4fecfe48f6bb90da6035558742e07
shreyaspranav <shrepranav5@gmail.com>
2023-03-11 13:26:46 +0530

    Added optick profiler support to OpenGLRenderer

--- Xenode/src/gfxapi/OpenGL/OpenGLRendererAPI.cpp
+++ Xenode/src/gfxapi/OpenGL/OpenGLRendererAPI.cpp
@@ -4,15 +4,18 @@
 #include <glad/gl.h>
 #include <core/app/Log.h>
 
+#include <core/app/Profiler.h>
 
 namespace Xen {
 	void OpenGLRendererAPI::Clear()
 	{
+		XEN_PROFILE_FN();
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 
 	void OpenGLRendererAPI::SetClearColor(const Color& color)
 	{
+		XEN_PROFILE_FN();
 		glClearColor(color.r, color.g, color.b, color.a);
 	}
 	void OpenGLRendererAPI::OnWindowResize(uint32_t width, uint32_t height)
@@ -21,11 +24,13 @@ namespace Xen {
 	}
 	void OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indices)
 	{
+		XEN_PROFILE_FN();
 		glDrawElements(GL_TRIANGLES, indices, GL_UNSIGNED_INT, 0);
 	}
-
+	
 	void OpenGLRendererAPI::DrawLines(const Ref<VertexArray>& vertexArray, uint32_t indices)
 	{
+		XEN_PROFILE_FN();
 		glDrawArrays(GL_LINES, 0, indices);
 	}
 	void OpenGLRendererAPI::SetLineWidth(float width)
