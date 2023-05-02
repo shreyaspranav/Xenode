55ceaa3adc2da5a050ee234dc31c569720c8f657
Shreyas Pranav <shrepranav5@gmail.com>
2023-05-02 13:42:03 +0530

    Showed demostration of EditorCamera in SandboxApp

--- SandboxApp/src/Test.h
+++ SandboxApp/src/Test.h
@@ -13,6 +13,7 @@
 #include <core/renderer/Camera.h>
 #include <core/renderer/Texture.h>
 #include <core/renderer/Structs.h>
+#include <core/scene/EditorCameraController.h>
 
 #include <core/app/Profiler.h>
 
@@ -24,13 +25,15 @@ public:
 
 	void OnAttach() override
 	{
-		m_Camera = std::make_shared<Xen::Camera>(Xen::CameraType::Orthographic, 
+		m_Camera = std::make_shared<Xen::Camera>(Xen::CameraType::Perspective, 
 			Xen::DesktopApplication::GetWindow()->GetFrameBufferWidth(), 
 			Xen::DesktopApplication::GetWindow()->GetFrameBufferHeight());
 
 		input = Xen::Input::GetInputInterface();
 		input->SetWindow(Xen::DesktopApplication::GetWindow());
 		Xen::Renderer2D::Init();
+
+		cam_control = Xen::EditorCameraController(input);
 	}
 
 	void OnDetach() override
@@ -42,26 +45,32 @@ public:
 	{
 		XEN_PROFILE_FN();
 
+		cam_pos = cam_control.GetCameraPosition();
+
+		bool active = true;
+		cam_control.Update(&active);
+
 		Xen::RenderCommand::Clear();
 		Xen::RenderCommand::SetClearColor(Xen::Color(0.13f));
 
 		if (zoom_iterations > 0)
 		{
-			m_Camera->SetScale(m_Camera->GetScale().z += scroll_direction * 0.03f);
+			//m_Camera->SetScale(m_Camera->GetScale().z += scroll_direction * 0.03f);
 			zoom_iterations--;
 		}
 
-		if (input->IsKeyPressed(Xen::KeyCode::KEY_A))
-			cam_pos.x -= 0.06 * timestep;
-		if (input->IsKeyPressed(Xen::KeyCode::KEY_D))
-			cam_pos.x += 0.06 * timestep;
-		if (input->IsKeyPressed(Xen::KeyCode::KEY_W))
-			cam_pos.y += 0.06 * timestep;
-		if (input->IsKeyPressed(Xen::KeyCode::KEY_S))
-			cam_pos.y -= 0.06 * timestep;
+		//if (input->IsKeyPressed(Xen::KeyCode::KEY_A))
+		//	cam_pos.x -= 0.06 * timestep;
+		//if (input->IsKeyPressed(Xen::KeyCode::KEY_D))
+		//	cam_pos.x += 0.06 * timestep;
+		//if (input->IsKeyPressed(Xen::KeyCode::KEY_W))
+		//	cam_pos.y += 0.06 * timestep;
+		//if (input->IsKeyPressed(Xen::KeyCode::KEY_S))
+		//	cam_pos.y -= 0.06 * timestep;
 
+		m_Camera->LookAtPoint(cam_control.GetFocalPoint());
 		m_Camera->SetPosition(cam_pos);
-		m_Camera->Update();
+		m_Camera->Update(1);
 
 		Xen::Renderer2D::BeginScene(m_Camera);
 		//for (int i = 0; i < 10; i++) {
@@ -112,4 +121,6 @@ private:
 	float cam_zoom = 1.0f;
 	uint32_t zoom_iterations = 0;
 	int scroll_direction = 0;
+
+	Xen::EditorCameraController cam_control;
 };
